a951d9f0f80b67dad5ef8f3d3c6081274bd95883
JustSabda <dataerror13@gmail.com>
2021-10-22 16:35:18 +0700

    Bukti Belajar Part 12

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static int score;
+    public Text scoreText;
+    public Text ammoText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score : " + score;
+        ammoText.text = WeaponController.ammo + "/" + WeaponController.ammoMag;
+    }
+    public void ResetScore()
+    {
+        score = 0;
+    }
+}
